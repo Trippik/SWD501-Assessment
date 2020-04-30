b7a8ae493269838b56d8ae7c217f4a68413feea8
Trippik <43576788+Trippik@users.noreply.github.com>
2020-04-30 21:41:07 +0100

    Add files via upload

--- Site/admin_dashboard.php
+++ Site/admin_dashboard.php
@@ -4,49 +4,42 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Dashboard</title>
+        <title>Admin Dashboard</title>
         <link href="style.css" rel="stylesheet" type="text/css">
     </head>
 <head>
-<title>User Dashboard</title>
+<title>Admin Dashboard</title>
 </head>
 <body>
     <?php
         session_start();
-        if (!isset ($_SESSION["login_passed"])):
+        if (($_SESSION["admin_state"]) != 1):
             echo "You shouldn't be here!!";
         else:
             $user = $_SESSION["gatekeeper"];
+            $db = new PDO('mysql:unix_socket=/tmp/mysql.sock;dbname=assign244', 'cam', 'cancan');
             echo '<table style="width:80%">
-                    <tr><th><h1>POI Dashboard</h1></th></tr>
+                    <tr><th><h1>POI Admin Dashboard</h1></th></tr>
                     <tr></tr>
-                    <tr><td>Welcome to POI ' . $user . ', please select from the options below</td></tr>
+                    <tr><td>Welcome to the admin dashboard ' . $user . ', please select from the options below</td></tr>
                 </table><br><br>';
-            echo '<form method="post" action="search_poi.php">
-                    <table style="width:80%">
-                        <tr><th>Search Points of Interest</th><tr>
-                        <tr></tr>
-                        <tr><td><input name="name" placeholder="Location Name" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td><input name="type" placeholder="Location Type (e.g city, pub, town)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td><input name="region" placeholder="Region (e.g Hampshire, Yorkshire, Sussex)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td><input name="country" placeholder="Country (e.g UK, France, USA)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td><input type="submit" value="Search!" /></td></tr>
-                    </table>
-                </form><br><br>';
-            echo '<form method="post" action="add_poi.php">
-                    <table style="width:80%">
-                        <tr><th colspan="2">Add a point of interest</th></tr>
-                        <tr></tr>
-                        <tr><td colspan="2"><input name="name" placeholder="Location Name" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td colspan="2"><input name="type" placeholder="Location Type (e.g city, pub, town)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td colspan="2"><input name="region" placeholder="Region (e.g Hampshire, Yorkshire, Sussex)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td colspan="2"><input name="country" placeholder="Country (e.g UK, France, USA)" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td><input name="longtitude" placeholder="Longtitude" style="display:table-cell; width:100%"/></td><td><input name="latitude" placeholder="Latitude" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td colspan="2"><input name="description" placeholder="Description" style="display:table-cell; width:100%"/></td></tr>
-                        <tr><td colspan="2"><input type="submit" value="Add new Point of Interest" /></td></tr>
-                    </table>
-                </form>';
+            $unapproved_reviews = $db->query('SELECT poi_reviews.id, pointsofinterest.name, pointsofinterest.type, pointsofinterest.description, poi_reviews.review FROM poi_reviews INNER JOIN pointsofinterest ON poi_reviews.poi_id = pointsofinterest.id WHERE poi_reviews.approved = 0');
+            echo '<table style="width:80%">
+                    <tr><th><h1>Results</h1></th></tr>
+                    <tr></tr>
+                </table><br><br>';
+            echo '<table style="width:80%">
+                    <tr><th>Name</th><th>Type</th><th>Description</th><th>Review</th></tr>';
+            while($row=$unapproved_reviews->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<tr><td>' . $row["name"] . '</td><td>' . $row["type"] . '</td><td>' . $row["description"] . '</td><td>' . $row["review"] . '</td><td style="width: 14em;"><form method="post" action="approve_review.php"><input type="hidden" name="review_id" value=' . $row["id"] . '><input type="submit" value="Approve Review" /></form></td></tr>';
+            }
+            echo '</table>';
         endif;
     ?>
+    <br><br>
+    <table>
+    <tr><td><button><a href = user_dashboard.php>Go to User Dashboard</a></button></td></tr>
+    </table>
 </body>
 </html>
